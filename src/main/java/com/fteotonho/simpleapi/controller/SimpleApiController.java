e240769e26b43b3caba6cf6cc52528074184a84f
fteotonho <frankson.teotonho@gmail.com>
2022-08-24 23:34:37 -0300

    Allowing CrossOrigin

--- src/main/java/com/fteotonho/simpleapi/controller/SimpleApiController.java
+++ src/main/java/com/fteotonho/simpleapi/controller/SimpleApiController.java
@@ -1,5 +1,6 @@
 package com.fteotonho.simpleapi.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SimpleApiController {
 
     @GetMapping("/api")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public String getGreetings() {
         return "Welcome to the Simple API!";
     }
